0fbb87253e06ce2ad6665418da04b0042b958d02
Ben Allen <ben.allen5@gmail.com>
2016-01-14 22:02:18 -0600

    Updated the hello-world example

--- hello-world/sketch.js
+++ hello-world/sketch.js
@@ -1,16 +1,28 @@
-// Learning Processing
-// Daniel Shiffman
-// http://www.learningprocessing.com
+/*
+This is a really simple example and
+illustrates a possible lesson in calling
+functions, passing parameters and reading
+documentation.
+*/
 
-// Example 1-1: stroke and fill
+var setup = function() {
+  createCanvas(300, 300);
+  noStroke();
+};
 
-function setup() {
-  createCanvas(480, 270);
-  stroke(0); 
-  fill(150);
-}
+var draw = function() {
+  //face
+  fill(255, 255, 0);
+  ellipse(150, 150, 200, 200);
 
-function draw() {
-  background(0, 0, 0);
-  rect(50,50,100,100);
-}
+  //eyes
+  fill(0, 0, 0);
+  ellipse(120, 120, 20, 20);
+  ellipse(180, 120, 20, 20);
+  
+  //mouth
+  fill(255, 0, 0);
+  // http://p5js.org/reference/#/p5/arc
+  arc(150, 150, 80, 150, 0, PI, CHORD);
+  // a simpler version of the smile could be achieved with a triangle or ellipse
+};
