2cb650283d505c91de5fd13b0e19c178ad8d134b
Ben Allen <ben.allen5@gmail.com>
2016-01-16 10:14:08 -0600

    Formatting change - made tab size 4 spaces

--- hello-world/sketch.js
+++ hello-world/sketch.js
@@ -6,23 +6,23 @@ documentation.
 */
 
 var setup = function() {
-  createCanvas(300, 300);
-  noStroke();
+  	createCanvas(300, 300);
+  	noStroke();
 };
 
 var draw = function() {
-  //face
-  fill(255, 255, 0);
-  ellipse(150, 150, 200, 200);
+	//face
+	fill(255, 255, 0);
+	ellipse(150, 150, 200, 200);
 
-  //eyes
-  fill(0, 0, 0);
-  ellipse(120, 120, 20, 20);
-  ellipse(180, 120, 20, 20);
+	//eyes
+	fill(0, 0, 0);
+	ellipse(120, 120, 20, 20);
+	ellipse(180, 120, 20, 20);
   
-  //mouth
-  fill(255, 0, 0);
-  // http://p5js.org/reference/#/p5/arc
-  arc(150, 150, 80, 150, 0, PI, CHORD);
-  // a simpler version of the smile could be achieved with a triangle or ellipse
+	//mouth
+	fill(255, 0, 0);
+	// http://p5js.org/reference/#/p5/arc
+	arc(150, 150, 80, 150, 0, PI, CHORD);
+	// a simpler version of the smile could be achieved with a triangle or ellipse
 };
