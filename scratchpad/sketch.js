182e268cda2552630b9a425796426460a6e8c99e
Ben Allen <ben.allen5@gmail.com>
2016-02-27 22:14:58 -0600

    2 eyeballs following the cursor

--- scratchpad/sketch.js
+++ scratchpad/sketch.js
@@ -3,40 +3,44 @@ var setup = function() {
     textSize(32);
 };
 
-var centerX = 120;
-var centerY = 120;
-var circleWidth = 100;
-var radius = circleWidth/2;
+var leftEyeX = 120;
+var leftEyeY = 120;
+var leftEyeCircleWidth = 100;
+var leftRadius = leftEyeCircleWidth/2;
+var rightEyeX = 220;
+var rightEyeY = 120;
+var rightEyeCircleWidth = 100;
+var rightRadius = rightEyeCircleWidth/2;
 
 var draw = function() {
     background(255);
     
     // eyes
     fill(255, 255, 255);
-    ellipse(centerX, centerY, circleWidth, 100);
+    ellipse(leftEyeX, leftEyeY,
+            leftEyeCircleWidth, leftEyeCircleWidth);
+    ellipse(rightEyeX, rightEyeY,
+            rightEyeCircleWidth, rightEyeCircleWidth);
 
     // eye balls
     fill(0, 0, 0);
-    ellipse(centerX, centerY, 20, 20);
+    drawEyeBall(leftEyeX, leftEyeY, leftRadius);
+    drawEyeBall(rightEyeX, rightEyeY, rightRadius);
+};
+
+var drawEyeBall = function(centerX, centerY, radius) {
+    var eyeBallSize = 30;
 
     var xDiff = mouseX - centerX;
     var yDiff = mouseY - centerY;
-    
+
     var hyp = sqrt(pow(xDiff, 2) + pow(yDiff, 2));
     
-    var proportion = radius/hyp;
+    var proportion = (radius-radius/2)/hyp;
 
     var xToFind = proportion * xDiff + centerX;
     var yToFind = proportion * yDiff + centerY;
 
-    fill(255, 0, 0);
-    ellipse(xToFind, yToFind, 10, 10);
-};
-
-// calculate the gradient of the line based on
-// the location of mouseX and mouseY
-var calculateM = function(){
-    relativeX = mouseX - centerX;
-    relativeY = mouseY - centerY;
-    return relativeY/relativeX;
-};
+    fill(0);
+    ellipse(xToFind, yToFind, eyeBallSize, eyeBallSize);
+}
